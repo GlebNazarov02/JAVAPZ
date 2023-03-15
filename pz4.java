80c426f42b7d13f91c0e0ece6d1b40649cecac89
Gelix1470 <gleb.nazarov.1982@mail,ru>
2023-03-15 16:45:04 +0300

    dz4

---
+++ pz4.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class pz4 {
+
+    public static final int SIZE = 5_000_000;
+
+    public static void main(String[] args) {
+        Integer[] data = createInitialArray(SIZE);
+
+        ArrayList<Integer> arrayList = createArrayList(data);
+        LinkedList<Integer> linkedList = createLinkedList(data);
+
+
+      
+    }
+
+    private static Integer[] createInitialArray(int size) {
+        Integer[] data = new Integer[size];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = ThreadLocalRandom.current().nextInt(1000);
+        }
+        return data;
+    }
+
+
+    static ArrayList<Integer> createArrayList(Integer[] data) {
+        return new ArrayList<>(List.of(data));
+    }
+
+    static LinkedList<Integer> createLinkedList(Integer[] data) {
+        return new LinkedList<>(List.of(data));
+    }
+
+}
